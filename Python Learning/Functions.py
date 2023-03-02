e37dfc7df8560d816748308aeaec1a9c74b8ade0
Saurav Kumar Sahoo <sauravkumar2021@vitbhopal.ac.in>
2023-03-02 10:43:14 +0530

    Star Added

--- Python Learning/Functions.py
+++ Python Learning/Functions.py
@@ -10,6 +10,9 @@ def tarea(height, base, type):
     else:
         return(height*base)
 
+def star(rows):
+    for i in range(rows):
+        print("*"*(i+1))
 
 # Driver Codes
 # -------------------
@@ -19,3 +22,7 @@ ty=int(input("Enter 1 for triangle (By default it is Rectangle)"))
 heig= int(input("Enter the Height of the Triangle: "))
 bas= int(input("Enter the Base of the Triangle: "))
 print(tarea(heig,bas,type=ty))
+
+#star Pattern
+rows= int(input("Enter the number of rows you want"))
+star(rows)
